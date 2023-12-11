dd23517506365dcf8752f961296f9e94e63c625e
JuanDangoSherpal <131016271+JuanDangoSherpal@users.noreply.github.com>
2023-12-11 18:02:52 -0500

    Two Sum

--- arrayProblems/arrays.py
+++ arrayProblems/arrays.py
@@ -2,12 +2,15 @@ def print_menu_arrays():
     print("Ejercicios de arrays y strings seleccione uno: ")
     print("217. Contains Duplicate")
     print("242. Valid Anagram")
+    print("1. Two sum")
 
     entrada = input("Seleccione el número de problema: ")
     if entrada == "217":
         contains_duplicate_menu()
     if entrada == "242":
         is_anagram_menu()
+    if entrada == "1":
+        two_sum_menu()
 
 #Todo lo relacionado con contains duplicate
 def contains_duplicate_menu():
@@ -94,3 +97,54 @@ def is_anagram(s, t):
             return False
         
     return True
+
+#Two sum problem
+def two_sum_menu():
+    s = input("Inserte los numeros separados por coma: ")
+
+    array = [int(i.strip()) for i in s.split(",")]
+    t = int(input("Ingrese el target: "))
+    print(twoSum(array, t))
+
+def twoSum(nums, target):
+    """
+        Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+        You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+        You can return the answer in any order.
+
+        
+
+        Example 1:
+
+        Input: nums = [2,7,11,15], target = 9
+        Output: [0,1]
+        Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+        Example 2:
+
+        Input: nums = [3,2,4], target = 6
+        Output: [1,2]
+        Example 3:
+
+        Input: nums = [3,3], target = 6
+        Output: [0,1]
+        
+
+        Constraints:
+
+        2 <= nums.length <= 104
+        -109 <= nums[i] <= 109
+        -109 <= target <= 109
+        Only one valid answer exists.
+        
+
+        Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+    """
+    values = {}
+    for i, num in enumerate(nums):
+        subs = target-num
+        if subs in values:
+            return [i, values[subs]]
+        else:
+            values[num] = i
